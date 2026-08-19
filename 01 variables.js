fd7edcdb3dac93af4900f5c63a26b5b81c23be98
Thirupathi <raminenithirupathiodl@gmail.com>
2026-08-19 20:42:20 +0530

    first commit

---
+++ 01 variables.js
@@ -0,0 +1,20 @@
+console.log("Hello, World!")
+
+const accountId = "123456"
+let accountEmail="Thiru@example.com"
+var accountPassword = "12345"
+
+accountCity = "Chennai"
+
+console.log("Account ID:", accountId);
+
+accountEmail = "hc@hc.com"
+accountPassword = "newpassword"
+accountCity = "Banglore"
+
+console.table([
+     accountId,
+     accountEmail,
+     accountPassword,
+    accountCity
+])
---
+++ 02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Hello, World!")
+
+let name = "Thiru"
+let age = 24
+let isLoggedIn = false
+let state = null
+
+//number => 2 to the power of 53
+//biginit
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value taken by a variable that is not yet defined
+//symbol => unique
+
+//object 
+
+console.log(typeof null)
+console.log(typeof undefined)
+
+
