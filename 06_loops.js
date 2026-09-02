5cc9fe5bd19b3b486cf7fa242977428bc3def685
Thirupathi <raminenithirupathiodl@gmail.com>
2026-09-02 09:13:27 +0530

    loops done

---
+++ 06_loops.js
@@ -0,0 +1,25 @@
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+for(let i=1;i<=3;i++){
+    console.log("count", i);
+}
+
+let i=0;
+while(i<3){
+    console.log("count", i);
+    i++;
+}
+
+for(let i=0;i<5;i++){
+    for(let j=0;j<i;j++){
+        console.log("thiru");
+    }
+}
+
+let j=0;
+do{
+    console.log("count", j);
+    j++;
+}while(j<3); 
