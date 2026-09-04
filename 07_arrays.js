6f9549ed0188b76963f785110675920fd1edf62d
Thirupathi <raminenithirupathiodl@gmail.com>
2026-09-04 07:08:40 +0530

    Modifications done in arrays

---
+++ 07_arrays.js
@@ -0,0 +1,27 @@
+let fruits =["apple", "banana", "cherry", "date", "orange"];
+
+console.log(fruits);
+
+console.log(fruits[2]);
+
+console.log(fruits.length);
+ 
+console.log(fruits[fruits.length - 1]);
+
+console.log(fruits)
+
+let fruit = new Array("kiwi", "mango", "papaya");
+
+console.log(fruit);
+
+fruit.push("guava");
+
+console.log(fruit);
+
+fruit.pop();
+
+console.log(fruit);
+
+fruit[2] = "guava";
+
+console.log(fruit);
